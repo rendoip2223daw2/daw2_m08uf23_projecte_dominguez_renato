c739e80e0b0e3caa5d3d6ab500146d519aa3269c
rendoip2223daw <rendoip2223Daw2@proton.me>
2023-04-26 18:44:07 +0200

    Commit Final

--- CRUD/esborrar.php
+++ CRUD/esborrar.php
@@ -10,7 +10,7 @@ use Laminas\Ldap\Ldap;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esborrar Usuari</title>
+    <title>Esborrar un Usuari</title>
     <style>
         <style>
         /* Estilos CSS personalizados */
